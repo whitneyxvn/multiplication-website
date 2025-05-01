068a49337a4edc65ed3ec86298aaf91740145e63
whitneyxvn <whitney.xv@gmail.com>
2025-05-01 19:25:16 +0100

    Create virtual recap.py

---
+++ virtual recap.py
@@ -0,0 +1,13 @@
+whitney= input("what is your name?")
+a= input("what age are you? ")
+school = input("what school do you go to?")
+language = input("what is your language?")
+
+# Displaying the virtual ID
+print("whitney")
+print("      VIRTUAL ID CARD")
+print("---------------------------")
+print(" your name is : " + whitney)
+print(" your age is  :" + a)
+print(" your school is: " + school)
+print(" your language is : " + language)
