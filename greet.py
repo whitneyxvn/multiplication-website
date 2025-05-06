6d30979c45ade328b1b11ffb9a832075cb287319
whitneyxvn <whitney.xv@gmail.com>
2025-05-06 18:45:19 +0100

    Update greet.py

--- greet.py
+++ greet.py
@@ -1,10 +1,11 @@
 name = input("What is your name? ")
 age = input("How old are you? ")
-favorite_color = input("What is your favorite color? ")
+favourite_color = input("What is your favourite color? ")
 current_mood = input("How are you feeling today? ")
-
+favourite_number = input("what is your favorite number")
+double_favourite_number = int(favourite_number) * 2
 # Display the friendly message
 print(f"\nHello {name}! 👋")
-print(f"You are {age} years old and love the color {favorite_color}.")
-print(f"You're feeling {current_mood} today — that’s awesome!")
-print("Remember: You’re amazing and the future is bright 🌟")
+print(f"You are {age} years old and love the color {favourite_color}.")
+print(f"You're feeling {current_mood} today — that’s awesome!") 
+print(f"your lucky number is {double_favourite_number} ! thats double your favorite number")
